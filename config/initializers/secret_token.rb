443285b2433aeeaded1af23643b2c632b1962903
Eric <eric.flecher@gmail.com>
2012-11-25 14:07:03 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Streamerhq::Application.config.secret_token = '62dbeec5dbeca6df81c477df967b1ea56d9d9ae406af70ae324e3d4f14ea0946f7bc5f157ce80b35051faba7b5aa0302991f96a87a5a4b880f8828ede72a2cac'
