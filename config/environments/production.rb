328670dd8eaa141b5eb22143f4584aa770743073
Eric <eric.flecher@gmail.com>
2012-12-12 22:49:29 -0600

    update host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Streamerhq::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'bckto.com' }
+  config.action_mailer.default_url_options = { :host => 'www.bckto.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
